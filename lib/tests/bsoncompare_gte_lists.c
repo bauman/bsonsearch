2e3fd496721fd6c8676ba4a7c3c7ea5b58d18972
dan <bauman.85@osu.edu>
2016-02-02 14:49:46 -0600

    $lt on a list of numbers

--- lib/tests/bsoncompare_gte_lists.c
+++ lib/tests/bsoncompare_gte_lists.c
@@ -40,6 +40,23 @@ int compare_gte(){
     return yes;
 }
 
+int compare_lt(){
+    bson_error_t error;
+    bson_error_t error2;
+    bson_t      *spec;
+    bson_t      *doc;
+
+    const char *json = "{\"hello\": [1,2,3]}";
+    const char *jsonspec = "{\"hello\": {\"$lt\":2}}";
+    doc = bson_new_from_json (json, -1, &error);
+    spec = bson_new_from_json (jsonspec, -1, &error2);
+    const uint8_t *spec_bson = bson_get_data(spec);
+    const uint8_t *doc_bson = bson_get_data(doc);
+    int yes = compare(spec_bson, spec->len, doc_bson, doc->len);
+    bson_free(spec);
+    bson_free(doc);
+    return yes;
+}
 
 int
 main (int   argc,
@@ -47,6 +64,7 @@ main (int   argc,
 {
     BSON_ASSERT(compare_gt());
     BSON_ASSERT(compare_gte());
+    BSON_ASSERT(compare_lt());
 
     return 0;
 }
