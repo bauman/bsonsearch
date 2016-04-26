05ac04118127ee385d674d79634ed4942bc2806b
dan <bauman.85@osu.edu>
2016-04-26 13:44:16 -0500

    python $project command fixing leak

--- test_project.py
+++ test_project.py
@@ -1,7 +1,9 @@
 from bsonsearch import bsoncompare
 bc = bsoncompare()
-doc = {"a":{"aa":["ii", 33]}, "b":"b"}
+doc = {"a":{"aa":[2, 33]}, "b":"b"}
 doc_id = bc.generate_doc(doc)
 spec = {"$project":{"a.aa":1}}
 query = bc.generate_matcher(spec)
-bc.project_bson(query, doc_id)
+while True:
+    result = bc.project_bson(query, doc_id) #should not memory leak the str
+
