fd668f430d6cd552f9fdee61d95fc9a8f8280516
Dan Bauman <b@uman.dev>
2025-11-03 13:55:57 -0500

    updates

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from os import environ
 
 include_dirs = [
     f"/usr/include/bson-{environ.get('BSON_VERSION', '2.1.2')}/",
-    f"/opt/homebrew/include/bson-{environ.get('BSON_VERSION', '2.1.2')}/"
+    f"/opt/homebrew/include/bson-{environ.get('BSON_VERSION', '2.1.2')}/",
+    "/opt/homebrew/include/"
 ]
 include_dirs.extend(environ.get("INCLUDE_DIR", ".").split(":"))
 lib_dirs = ["/opt/homebrew/lib/"]
